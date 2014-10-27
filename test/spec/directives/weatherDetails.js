bfaa103c84b79d201499e381f168e9492d13157a
Nemanja Popovic <nemanja23031991@yahoo.com>
2014-10-27 14:19:53 +0100

    Removed test cases

--- test/spec/directives/weatherDetails.js
+++ test/spec/directives/weatherDetails.js
@@ -12,9 +12,4 @@ describe('Directive: weatherDetails', function () {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<weather-details></weather-details>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the weatherDetails directive');
-  }));
 });
